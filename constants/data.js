747da39a6cd913e9af1db0a67093d41832ec59d9
wnesbitt <jlm457@mocs.utc.edu>
2021-09-20 19:53:55 -0400

    MAINT: updated about info

--- constants/data.js
+++ constants/data.js
@@ -20,14 +20,14 @@ const userData = {
   ],
   about: {
     title:
-      "I'm a software engineer that loves building products and web applications that impact millions of lives",
+      "I'm a software engineer that",
     description: [
-      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
-      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
-      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+      `I've been developing.`,
+      `After learning.`,
+      `But now, .`,
     ],
     currentProject: "RADFX",
-    currentProjectUrl: "https://tailwindmasterkit.com",
+    currentProjectUrl: "https://RADFX.Vercel.app",
   },
   experience: [
     {
@@ -42,24 +42,19 @@ const userData = {
       company: "First National Bank of Pulaski",
       year: "2017",
       companyLink: "https://www.fnbforyou.com",
-      //desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
     },
     {
       title: "High School",
       company: "Giles County High, Pulaski.",
       year: "2017",
       companyLink: "https://www.gcboe.us/GCHS",
-      //desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
     },
   ],
   resumeUrl:
-    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+    "https://google.com",
   socialLinks: {
-    //instagram: "https://instagram.com/maninthere",
-    //twitter: "https://twitter.com/mannupaaji",
     linkedin: "https://www.linkedin.com/in/wesley-nesbitt",
     github: "https://github.com/wnesbitt",
-    //facebook: "https://facebook.com/",
   },
 };
 
