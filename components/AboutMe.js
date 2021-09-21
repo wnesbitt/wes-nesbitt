a53f3bfc5f700d4936a631956de3035a3463d56e
wnesbitt <jlm457@mocs.utc.edu>
2021-09-20 20:42:25 -0400

    MAINT: updated index

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -20,7 +20,7 @@ export default function AboutMe() {
               className="bg-red-500 rounded-md px-2 py-1 text-white"
               href={userData.about.currentProjectUrl}
             >
-              {userData.about.currentProject} ✈️
+              {userData.about.currentProject} 🚀
             </a>
           </p>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import ContainerBlock from "../components/ContainerBlock";
-//import FavouriteProjects from "../components/FavouriteProjects";
+import FavouriteProjects from "../components/FavouriteProjects";
 import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
 import getLatestRepos from "@lib/getLatestRepos";
@@ -14,8 +14,8 @@ export default function Home({ repositories }) {
       description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
     >
       <Hero />
-      {/* <FavouriteProjects /> */}
-      {/* <LatestCode repositories={repositories} /> */}
+      <FavouriteProjects />
+      <LatestCode repositories={repositories} />
     </ContainerBlock>
   );
 }
